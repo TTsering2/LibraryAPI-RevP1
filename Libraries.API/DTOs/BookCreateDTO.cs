711ad10b8a53cf10ccdad599af21838b455bba8e
Tsering Tsering <dtseringt2@gmail.com>
2024-04-18 19:33:52 -0400

    'DTO'

--- Libraries.API/DTOs/BookCreateDTO.cs
+++ Libraries.API/DTOs/BookCreateDTO.cs
@@ -2,7 +2,7 @@ namespace Libraries.API.DTOs;
 
 public class BookCreateDTO 
 {
-     public string Title { get; set; }
+    public string Title { get; set; }
     public string Summary { get; set; }
     public string AuthorName { get; set; }
     public string GenreName { get; set; }
